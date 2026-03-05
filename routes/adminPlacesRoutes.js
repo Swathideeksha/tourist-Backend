77ce9fda21a689b0079ff229660456eec73de1cb
sushhbangera <sushmithard8@gmail.com>
2026-03-05 09:35:09 +0530

    Updated backend

--- routes/adminPlacesRoutes.js
+++ routes/adminPlacesRoutes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
 const Place = require("../models/Place");
 
 // Get all places from database
